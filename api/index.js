71dd1ad6f1a97f84f2508ae87344d09ea596e7ca
asepsaefuddin <githubasep@gmail.com>
2026-04-24 22:05:37 +0700

    five

--- api/index.js
+++ api/index.js
@@ -5,6 +5,15 @@ const { connectDB } = require('../src/config/db');
 const handler = serverless(app);
 
 module.exports = async (req, res) => {
-  await connectDB();
+  try {
+    await connectDB();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: 'Database connection failed',
+      error: err.message
+    });
+  }
+
   return handler(req, res);
 };
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 let cachedConnection = null;
+let cachedPromise = null;
 
 async function connectDB() {
   if (cachedConnection) {
     return cachedConnection;
+  }
+  if (cachedPromise) {
+    return cachedPromise;
   }
 
   const uri = process.env.MONGO_URI;
@@ -12,8 +16,20 @@ async function connectDB() {
     throw new Error('MONGO_URI is not set');
   }
 
-  cachedConnection = await mongoose.connect(uri);
-  return cachedConnection;
+  // Fail fast in serverless to avoid hitting function timeout.
+  cachedPromise = mongoose.connect(uri, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 5000,
+    socketTimeoutMS: 10000
+  });
+
+  try {
+    cachedConnection = await cachedPromise;
+    return cachedConnection;
+  } catch (err) {
+    cachedPromise = null;
+    throw err;
+  }
 }
 
 module.exports = { connectDB };
